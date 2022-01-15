05d002eb1bd74370e4ac562ebdc44a1fcb3ec2a5
Suhail Ahmad <97229491+suhaillahmad@users.noreply.github.com>
2022-01-15 23:45:31 +0530

    Update pattern15.cpp

--- assigment solution in c++/pattern15.cpp
+++ assigment solution in c++/pattern15.cpp
@@ -11,15 +11,15 @@ int s=2*n-1;
 			cout<<m;
 			m--;
 		}
-		for(int k=0;k<s-2*i;k++){
-			cout<<n-i;
-		}
-		m=n-i+1;
-		for(int l=0;l<i;l++){
-			cout<<m;
-			m++;
-		}
-		cout<<endl;
+		for(int k=0;k<s-2*i;k++){       //. 5 5 5 5 5 5 5 5 5
+			cout<<n-i;		//. 5 4 4 4 4 4 4 4 5
+		}				//. 5 4 3 3 3 3 3 4 5
+		m=n-i+1;			//. 5 4 3 2 2 2 3 4 5
+		for(int l=0;l<i;l++){		//. 5 4 3 2 1 2 3 4 5
+			cout<<m;		//. 5 4 3 2 2 2 3 4 5
+			m++;			//. 5 4 3 3 3 3 3 4 5
+		}				//. 5 4 4 4 4 4 4 4 5
+		cout<<endl;			//. 5 5 5 5 5 5 5 5 5
 	}
 	for(int i=s/2-1;i>=0;i--){
 		int m=n;
